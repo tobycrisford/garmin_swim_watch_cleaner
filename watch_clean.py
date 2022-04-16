59537db71bbbd5c4201e8ce63a030808af7ec467
Toby Crisford <tobycrisford@gmail.com>
2022-04-16 13:37:03 +0100

    Making it certain that length transitions will be missed in the 10 seconds following a spurious transition

--- watch_clean.py
+++ watch_clean.py
@@ -11,7 +11,7 @@ import copy
 
 class Length:
     
-    def __init__(self, start_time, finish_time, next_length, start_recorded, end_recorded, length_label, all_recorded, index_start, index_stop):
+    def __init__(self, start_time, finish_time, next_length, start_recorded, end_recorded, length_label, all_recorded, index_start, index_stop, start_moved):
         #index_start/stop gives the lowest, highest index of all_recorded in [...) (if stop < start this indicates no values)
         self.start_time = start_time
         self.finish_time = finish_time
@@ -23,6 +23,7 @@ class Length:
         self.all_recorded = all_recorded
         self.index_start = index_start
         self.index_stop = index_stop
+        self.start_moved = start_moved
     
     @classmethod
     def create_from_recorded(cls, all_recorded, start_index=0):
@@ -32,15 +33,18 @@ class Length:
         else:
             next_length = None
         
-        return Length(all_recorded[start_index], all_recorded[start_index + 1], next_length, True, True, length_label, all_recorded, start_index, start_index)
+        return Length(all_recorded[start_index], all_recorded[start_index + 1], next_length, True, True, length_label, all_recorded, start_index, start_index, False)
               
     #This function is expensive, run only once at start, then each length modifies global state in place as required
     def calculate_global_state(self, global_state=None):
         
         if global_state is None:
-            global_state = {'missed': 0, 'recorded': 0, 'n': np.array([0,0]), 'extra': 0, 'mean': np.array([0.0,0.0]), 'meansquare': np.array([0.0,0.0])}
+            global_state = {'missed': 0, 'recorded': 0, 'n': np.array([0,0]), 'extra': 0, 'mean': np.array([0.0,0.0]), 'meansquare': np.array([0.0,0.0]), 'moved_starts': 0}
         
-        global_state['missed'] += (not self.start_recorded)
+        if not self.start_moved:
+            global_state['missed'] += (not self.start_recorded)
+        else:
+            global_state['moved_starts'] += 1
         if self.next_length is None:
             global_state['missed'] += (not self.end_recorded)
         if not (self.next_length is None):
@@ -78,8 +82,9 @@ class Length:
         
         return s_prob_factors, potential_mean, potential_meansquare, potential_S, potential_var
     
-    def add_missing(self, potential_missed, new_start_index, new_stop_index, time_factor, beta):
-        prob_factor = (self.global_state['missed'] + 1) / (self.global_state['missed'] + self.global_state['recorded'] + (beta-1) + 2)
+    def add_missing(self, potential_missed, new_start_index, new_stop_index, time_factor, beta, near_extra):
+        if not near_extra:
+            prob_factor = (self.global_state['missed'] + 1) / (self.global_state['missed'] + self.global_state['recorded'] + (beta-1) + 2)
         prob_factor = prob_factor * ((self.global_state['n'] + 1) / (np.sum(self.global_state['n']) + 2))
                 
         s_prob_factors, potential_mean, potential_meansquare, potential_S, potential_var = self.s_effect_of_new_add(potential_missed)
@@ -100,7 +105,7 @@ class Length:
                 new_label = 1
             self.next_length = Length(potential_missed, self.finish_time,
                                       self.next_length, False, self.end_recorded,
-                                      new_label, self.all_recorded, new_start_index, self.index_stop)
+                                      new_label, self.all_recorded, new_start_index, self.index_stop, True)
             self.next_length.global_state = self.global_state
             self.finish_time = potential_missed
             self.duration = self.finish_time - self.start_time
@@ -108,7 +113,10 @@ class Length:
             self.index_stop = new_stop_index #If < self.index_start this indicates no recorded points in [)
                     
             #Finally need to increment global state
-            self.global_state['missed'] += 1
+            if near_extra:
+                self.global_state['moved_starts'] += 1
+            else:
+                self.global_state['missed'] += 1
             self.global_state['n'][new_label] += 1
             self.global_state['mean'] = potential_mean[new_label,:]
             self.global_state['meansquare'] = potential_meansquare[new_label,:]
@@ -180,11 +188,13 @@ class Length:
             #Was a length transition missed?
             if i == self.index_start:
                 lb = self.start_time
+                lb_extra = False
             else:
                 lb = self.all_recorded[i-1]
+                lb_extra = True
             if self.all_recorded[i] > lb:
                 potential_missed = np.random.uniform(lb, self.all_recorded[i])
-                add_missing_test = self.add_missing(potential_missed, i, i - 1, self.all_recorded[i] - lb, beta)
+                add_missing_test = self.add_missing(potential_missed, i, i - 1, self.all_recorded[i] - lb, beta, (potential_missed - lb < 10) and lb_extra)
                 if not (add_missing_test is None):
                     return add_missing_test
                 
@@ -207,6 +217,11 @@ class Length:
                 
                 prob_factor *= self.all_recorded[-1] - self.all_recorded[0]
                 
+                if i == self.index_stop and (not (self.next_length is None)):
+                    if self.next_length.start_moved and self.next_length.start_time - self.all_recorded[i] < 10:
+                        prob_factor *= (self.global_state['missed'] + 1) / (self.global_state['missed'] + (self.global_state['recorded'] + 1) + (beta-1) + 2)
+                        
+                
                 prob_stick = 1 / (1 + np.sum(prob_factor))
                 rand_number = np.random.rand()
                 if rand_number > prob_stick:
@@ -214,9 +229,14 @@ class Length:
                         new_label = 0
                     else:
                         new_label = 1
+                    if i == self.index_stop and (not (self.next_length is None)):
+                        if self.next_length.start_moved and self.next_length.start_time - self.all_recorded[i] < 10:
+                            self.next_length.start_moved = False
+                            self.global_state['missed'] += 1
+                            self.global_state['moved_starts'] -= 1
                     self.next_length = Length(self.all_recorded[i], self.finish_time,
                                               self.next_length, True, self.end_recorded,
-                                              new_label, self.all_recorded, i, self.index_stop)
+                                              new_label, self.all_recorded, i, self.index_stop, False)
                     self.next_length.global_state = self.global_state
                     self.finish_time = self.all_recorded[i]
                     self.duration = self.finish_time - self.start_time
@@ -237,10 +257,11 @@ class Length:
         #Was a length transition missed near the end?
         if self.index_stop < self.index_start:
             potential_missed = np.random.uniform(self.start_time, self.finish_time)
-            add_missing_test = self.add_missing(potential_missed, self.index_stop + 1, self.index_stop, self.finish_time - self.start_time, beta)
+            add_missing_test = self.add_missing(potential_missed, self.index_stop + 1, self.index_stop, self.finish_time - self.start_time, beta, False)
         else:
             potential_missed = np.random.uniform(self.all_recorded[self.index_stop], self.finish_time)
-            add_missing_test = self.add_missing(potential_missed, self.index_stop + 1, self.index_stop, self.finish_time - self.all_recorded[self.index_stop], beta)
+            add_missing_test = self.add_missing(potential_missed, self.index_stop + 1, self.index_stop, self.finish_time - self.all_recorded[self.index_stop], beta,
+                                                self.all_recorded[self.index_stop] != self.start_time and potential_missed - self.all_recorded[self.index_stop] < 10)
         if not (add_missing_test is None):
             return add_missing_test
         
@@ -253,8 +274,13 @@ class Length:
                 else:
                     prob_factor *= self.global_state['extra'] / (self.global_state['extra'] + 1)
                 prob_factor *= 1 / (self.all_recorded[-1] - self.all_recorded[0])
+                if (not self.next_length.end_recorded) and self.next_length.duration < 10 and self.next_length.index_start == self.next_length.index_stop:
+                    prob_factor *= (self.global_state['missed'] + (self.global_state['recorded']-1) + 1 + (beta-1)) / (self.global_state['missed'])
             else:
-                prob_factor = (self.global_state['missed']+self.global_state['recorded']+1+(beta-1))/(self.global_state['missed'])
+                if self.next_length.start_moved:
+                    prob_factor = 1.0
+                else:
+                    prob_factor = (self.global_state['missed']+self.global_state['recorded']+1+(beta-1))/(self.global_state['missed'])
                 if self.next_length.index_stop < self.next_length.index_start:
                     ub = self.next_length.finish_time
                 else:
@@ -301,8 +327,15 @@ class Length:
                 if self.end_recorded:
                     self.global_state['recorded'] -= 1
                     self.global_state['extra'] += 1
+                    if (not self.next_length.end_recorded) and self.next_length.duration < 10 and self.next_length.index_start == self.next_length.index_stop:
+                        self.global_state['missed'] += 1
+                        self.global_state['moved_starts'] -= 1
+                        self.next_length.next_length.start_moved = False
                 else:
-                    self.global_state['missed'] -= 1
+                    if self.next_length.start_moved:
+                        self.global_state['moved_starts'] -= 1
+                    else:
+                        self.global_state['missed'] -= 1
                 self.global_state['n'][self.next_length.length_label] -= 1
                 self.global_state['mean'] = potential_mean
                 self.global_state['meansquare'] = potential_meansquare
